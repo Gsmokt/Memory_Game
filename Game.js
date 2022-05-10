f1ace3211618dc1906b21a272f33d437acb19415
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-05-10 10:11:21 +0200

    new commit

--- Game.js
+++ Game.js
@@ -4,24 +4,45 @@ export class Game {
   constructor(board) {
     this.board = board;
     this.numbersOfPlayers = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
-    this.players = [];
-    this.results;
+    this.players;
+    this.results = {};
   }
   getRandomPlayers() {
     this.players = this.board
       .slice(0, this.numbersOfPlayers)
       .flatMap((_, i) => new Players(i + 1));
   }
+  checkIfGuess(x) {
+    if (x > this.players.length - 1 || this.board.length === 0) return;
+    const random = this.players[x].memorizedCards(this.board);
+    this.board = random !== false ? random : this.board;
+    this.results = {
+      ...this.results,
+      [`Player ${this.players[x].name} number_of_Hits`]:
+        this.players[x].pairs.length,
+    };
+    return this.checkIfGuess(++x);
+  }
+
   startGame() {
-    if (this.players.length === 0) this.getRandomPlayers();
+    !this.players && this.getRandomPlayers();
     if (this.board.length === 0) {
       throw this.results;
     }
-    this.results = this.players.map((player) => {
-      const random = player.memorizedCards(this.board);
-      this.board = random !== false ? random : this.board;
-      return { Player: player.name, Number_of_Hits: player.pairs.length };
-    });
+    this.checkIfGuess(0);
     return this.startGame();
   }
 }
+
+
+
+
+
+
+
+// this.results = this.players.map((player) => {
+//   const random = player.memorizedCards(this.board);
+//   this.board = random !== false ? random : this.board;
+//   return { Player: player.name, Number_of_Hits: player.pairs.length };
+// });
+// .slice(this.results.length - this.players.length, this.results.length);
